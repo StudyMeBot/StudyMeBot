c95d2c4a18f3f50f654f7da882e5b12e23d6353f
StudyMeBot <studymebot@gmail.com>
2025-05-30 19:49:47 +0900

    feat: add noon push notification script

--- noon_push.py
+++ noon_push.py
@@ -13,7 +13,7 @@ line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 USER_ID = "U8a4ecdbbd8a9e27421ef720554900839"
 
 # 通知メッセージ
-message = TextSendMessage(text="🌙 こんばんは！今日の勉強を少しだけでも振り返ってみましょう📘")
+message = TextSendMessage(text="🍱 お昼です！午後も集中して取り組んでいきましょう✍️")
 
 # メッセージ送信
 line_bot_api.push_message(USER_ID, message)
